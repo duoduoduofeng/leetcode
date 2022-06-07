70430ae4996ad33184767700f517ac57f8ad5943
FENG DUO <duoduo@FENGdeMacBook-Pro.local>
2022-06-07 13:07:55 +0800

    Add S_019_RemoveNthNode.

---
+++ src/leetcodeproblems/LC_001_100/S_019_RemoveNthNode.java
@@ -0,0 +1,53 @@
+package leetcodeproblems.LC_001_100;
+
+import datastructures.ListNode;
+
+public class S_019_RemoveNthNode {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        // 题目已经保证了 1<=n<=size
+        if(head == null || head.next == null) {
+            return null;
+        }
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+        ListNode helper = new ListNode(-1);
+        ListNode dummy = helper;
+        // This design is very important because it can cover the condition n=size.
+        helper.next = head;
+
+        int steps = 0;
+        while(fast != null && fast.next != null && steps < n - 1) {
+            fast = fast.next;
+            ++steps;
+        }
+
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+            helper = helper.next;
+        }
+
+        helper.next = slow.next;
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args) {
+        S_019_RemoveNthNode ex = new S_019_RemoveNthNode();
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+//        head.next.next = new ListNode(3);
+//        head.next.next.next = new ListNode(4);
+//        head.next.next.next.next = new ListNode(5);
+
+        int n = 2;
+        ListNode newhead = ex.removeNthFromEnd(head, n);
+
+        while(newhead != null) {
+            System.out.println(newhead.val);
+            newhead = newhead.next;
+        }
+    }
+}
